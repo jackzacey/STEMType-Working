ef50652f4d6d41777972cbeb531a3282f7091955
jackzacey <jackzacey@gmail.com>
2025-11-23 20:33:17 -0800

    Create RootLayout component with metadata

---
+++ app/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'STEMType',
+  description: 'Master Bio & Chem by typing',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-bg text-text">{children}</body>
+    </html>
+  );
+}
